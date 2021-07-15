3abb672cfc547e31b427e7050bf7bb08afbf2b8c
Michel Albert <michel@albert.lu>
2021-07-15 16:06:54 +0200

    Add zshinit for WSL

--- zsh-perhost/DESKTOP-BHQ0QLD.zsh
+++ zsh-perhost/DESKTOP-BHQ0QLD.zsh
@@ -1,4 +1,4 @@
-eval `keychain --eval MichelAlbert-private-2016`
+eval `keychain --eval --agents ssh,gpg MichelAlbert-private-2016 B594F110`
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 export GPG_TTY=$(tty)
