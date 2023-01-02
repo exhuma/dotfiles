ceef71150046c6c4e170fe38f932978802501815
Michel Albert <michel.albert@post.lu>
2023-01-02 08:29:33 +0100

    Add completions & snippet engine to nvim

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -14,10 +14,13 @@ local on_attach = function(_, _)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 end
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require('lspconfig').sumneko_lua.setup({
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
 })
 require('lspconfig').pyright.setup({
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
 })
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -28,7 +28,12 @@ return require('packer').startup(function(use)
       'williamboman/mason-lspconfig.nvim',
       'neovim/nvim-lspconfig',
   }
-
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
